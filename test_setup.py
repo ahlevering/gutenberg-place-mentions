ffdebba06b8ba7015f1fcbbe74d82b8fbc01e3d2
Bixbeat <ahlevering@hotmail.com>
2017-04-22 16:39:08 +0200

    Added metadata retrieval of ebooks

--- test_setup.py
+++ test_setup.py
@@ -6,27 +6,46 @@
 """
 
 from os import chdir
-chdir("D:\cygwinfolders\gutenberg-generated")
 import epub_conversion
 from epub_conversion.utils import open_book
+import zipfile
+from lxml import etree
+
+# Set to your e-book root folder
+chdir("D:\cygwinfolders\gutenberg-generated")
 
 book = open_book(r'D:\cygwinfolders\gutenberg-generated\25014\pg25014.epub')
 
 lines = epub_conversion.converter.convert_epub_to_lines(book)
 
-"""
-from ebooklib import epub
-book = epub.read_epub(r'D:\cygwinfolders\gutenberg-generated\25014\pg25014.epub')
-book._id_html
-for i in book.get_items:
-    print(i)
-"""
+# Script retrieved from the following location:
+# http://stackoverflow.com/questions/3114786/python-library-to-extract-epub-information
+def get_epub_info(fname):
+    ns = {
+        'n':'urn:oasis:names:tc:opendocument:xmlns:container',
+        'pkg':'http://www.idpf.org/2007/opf',
+        'dc':'http://purl.org/dc/elements/1.1/'
+    }
 
-"""
-import epub
-book = epub.open_epub(r'D:\cygwinfolders\gutenberg-generated\25014\pg25014.epub')
+    # prepare to read from the .epub file
+    zip = zipfile.ZipFile(fname)
 
-for item in book.opf.manifest.values():
-    # read the content
-    data = book.read_item(item)
-"""
+    # find the contents metafile
+    txt = zip.read('META-INF/container.xml')
+    tree = etree.fromstring(txt)
+    cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path',namespaces=ns)[0]
+
+    # grab the metadata block from the contents metafile
+    cf = zip.read(cfname)
+    tree = etree.fromstring(cf)
+    p = tree.xpath('/pkg:package/pkg:metadata',namespaces=ns)[0]
+    return(p)
+    
+    # repackage the data
+    res = {}
+    for s in ['title','language','creator','date','identifier']:
+        res[s] = p.xpath('dc:%s/text()'%(s),namespaces=ns)[0]
+
+    return res
+    
+metadata = get_epub_info(r'D:\cygwinfolders\gutenberg-generated\1\pg1.epub')
